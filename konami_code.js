28c0b4a10a9ff7253809e379c42b9b1685c45bc0
Austin Blake <ummwhere@hotmail.com>
2017-12-08 22:38:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  const alphabet = code;
-
   let index = 0;
 
-  function onKeyDownHandler (e) {
-    const key = parseInt(e.detail || e.which);
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
 
-    if (key === alphabet[index]) {
-      index ++;
+    if (code[index] === key) {
+      index++;
 
-      if (index === alphabet.length) {
-        alert ('Hurray!');
+      if (index === code.length) {
+        alert('Hurray!');
 
         index = 0;
       }
-
-    }else {
+    } else {
       index = 0;
     }
-  }
-
+  });
 }
